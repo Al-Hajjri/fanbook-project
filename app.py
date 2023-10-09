338dec26b3153bc41558af294dd9f3550eb50a80
alhajjri <alhajjriprimasaputra@gmail.com>
2023-10-09 16:18:04 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print('hello ')
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://alhajjri:SamsungA7@cluster0.g8buonf.mongodb.net/?retryWrites=true&w=majority')
+
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
